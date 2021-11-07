45cd938ecd78352a53c4b51c3da0e9ccbadb46f0
Ayden Brown <pausesky43@gmail.com>
2021-11-06 21:12:15 -0600

    created functions for returning random letters, numbers, and symbols

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowercase = "abcdefghijklmnopqrstuvwxyz"
+const numbers = "0123456789"
+const symbols = "!@#$%^&*+_-="
+
+function getRandomUpperCase() {
+  return uppercase[Math.floor(Math.random() * uppercase.length)];
+}
+
+function getRandomLowerCase() {
+  return lowercase[Math.floor(Math.random() * lowercase.length)];
+}
+
+function getRandomNumber() {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getRandomSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePassword() {
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
